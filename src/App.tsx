305e067ffe2aa9a5639d153511aa06ce10c9dc71
marwen <marwen.mrabti.me@gmail.com>
2024-12-07 09:36:12 +0100

    ci: ts-query setup + body styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import { reactLogo, tailwindLogo } from "./assets"
+import { reactLogo, tsQueryLogo } from "./assets";
 
 function App() {
   return (
-    <main className='max-w-dvw min-h-dvh  bg-slate-900'>
-      <img src={reactLogo} alt="" />
-      <img src={tailwindLogo} alt="" />
-        <h1 className='text-white text-4xl'>tailwind</h1>
-      </main>
-  )
+    <main className="min-h-dvh w-screen bg-transparent">
+      <div className="grid w-full max-w-7xl grid-cols-1 place-items-center gap-4 px-4 py-2">
+        <img src={reactLogo} alt="react.js logo" />
+        <img src={tsQueryLogo} alt="tanstack query logo" />
+        <h1 className="text-primary text-4xl">tanstack query</h1>
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,2 +1,2 @@
-export { default as reactLogo } from './react.svg';
-export { default as tailwindLogo } from './tailwindcss.svg';
+export { default as reactLogo } from "./react.svg";
+export { default as tsQueryLogo } from "./ts-query.svg";
